35463b3a4f46fdbd835241d280d67823077a7f32
jallend1 <jallend1@gmail.com>
2020-12-17 15:46:37 -0800

    Creates basic structure for SignUp component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         <NavBar />
         <Switch>
           <Route exact path='/' component={Dashboard} />
-          <Route path="/signin" component={SignIn} />
+          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
           <Route path='/project/:id' component={ProjectDetails} />
         </Switch>
       </div>
