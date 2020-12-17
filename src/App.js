36a9b24f6a073a251309d0b588a6435e519c9a44
jallend1 <jallend1@gmail.com>
2020-12-17 15:39:35 -0800

    Creates SignIn component and incorporates Materialize JavaScript CDN

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
 import NavBar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <NavBar />
         <Switch>
           <Route exact path='/' component={Dashboard} />
+          <Route path="/signin" component={SignIn} />
           <Route path='/project/:id' component={ProjectDetails} />
         </Switch>
       </div>
