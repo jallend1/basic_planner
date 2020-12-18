b7fa415c90f6a17c091156b7e5e9fb4d49e42601
jallend1 <jallend1@gmail.com>
2020-12-17 16:04:39 -0800

    Completes basic CreateProject component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import NavBar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails';
+import CreateProject from './components/projects/CreateProject';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 
@@ -14,9 +15,10 @@ function App() {
         <NavBar />
         <Switch>
           <Route exact path='/' component={Dashboard} />
+          <Route path='/create' component={CreateProject} />
+          <Route path='/project/:id' component={ProjectDetails} />
           <Route path='/signin' component={SignIn} />
           <Route path='/signup' component={SignUp} />
-          <Route path='/project/:id' component={ProjectDetails} />
         </Switch>
       </div>
     </BrowserRouter>
