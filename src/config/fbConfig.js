2075678986cf4448e952adeb37ebd5fcbda09fc3
jallend1 <jallend1@gmail.com>
2020-12-19 13:25:09 -0800

    Sets up Firebase in app and ignores file with Firebase credentials

---
+++ src/config/fbConfig.js
@@ -0,0 +1,13 @@
+//Personal project's Firebase credentials
+import firebaseConfig from 'firebase.js';
+
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsinSnapshots: true});
+
+export default firebase;
